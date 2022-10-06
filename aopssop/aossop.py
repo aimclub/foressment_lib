b965c606db92680eaaf293803a2983d1540bdee6
descom9 <106529714+descom9@users.noreply.github.com>
2022-10-06 12:59:34 +0300

    Update aossop.py

--- aopssop/aossop.py
+++ aopssop/aossop.py
@@ -2,9 +2,13 @@
 # и процессов, включающего наличие или отсутствие в текущий момент определенного вида
 # функциональных неисправностей, дефектов и атакующих воздействий, свойственных целевой системе
 
+import  subprocess
+import sys
 
 def aossop(data):
     data += '\naossop:'
     # just to push smth
+
+    subprocess.Popen(['python3', 'classify_records.py', '-f', sys.argv[1], '-d', sys.argv[2]])
 
     return data
