9b9f869b5dcec718a1c26312b5751905af9778df
levshun <dmitry.levshun@gmail.com>
2022-10-06 18:56:09 +0300

    Datasets folder, assessment/forecasting renaming

--- aopssop/__init__.py
+++ aopssop/__init__.py
@@ -1,5 +1,5 @@
 # Short import definition
 from aopssop.preprocessing.posnd.posnd import posnd
 from aopssop.preprocessing.izsnd import izsnd
-from aopssop.evaluation.aossop.aossop import aossop
-from aopssop.prediction.apssop import apssop
+from aopssop.assessment.aossop.aossop import aossop
+from aopssop.forecasting.apssop import apssop
