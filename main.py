271f0405cffdd856c25d7edb568425b53ffd5515
levshun <dmitry.levshun@gmail.com>
2022-10-06 17:25:41 +0300

    New structure

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import aopssop
 
 
 if __name__ == '__main__':
-    # print(aopssop.posnd('Hello World!'))
+    print(aopssop.posnd('Hello World!'))
     # print(aopssop.izsnd('Hello World!'))
-    print(aopssop.aossop('Hello World!'))
+    # print(aopssop.aossop('Hello World!'))
     # print(aopssop.apssop('Hello World!'))
 
