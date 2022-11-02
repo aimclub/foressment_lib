e96aa6ca0dddc4b99048b103625748ded6598768
descom9 <106529714+descom9@users.noreply.github.com>
2022-11-02 18:26:01 +0300

    Delete aossop_tests.py

--- tests/aossop_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from aopssop import aossop
-
-
-class AossopTestCase(unittest.TestCase):
-
-    def test_smth(self):
-        result = aossop('Hello World!')
-        self.assertEqual(result, 'smth')
