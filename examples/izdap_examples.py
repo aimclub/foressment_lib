3400c88f11b44d72895d7fb22474ac34a2124fae
tushkanova <tushkanova.on@gmail.com>
2022-12-01 00:19:44 +0300

    izdap example fix

--- examples/izdap_examples.py
+++ examples/izdap_examples.py
@@ -6,13 +6,18 @@ if __name__ == '__main__':
     
     if sys.argv:
         
-        test_path = sys.argv[0]
-        class_column = sys.argv[1]
-        positive_class_label = sys.argv[2]
+        data = make_classification(n_samples=200, n_features=4, 
+                                   n_informative=2, n_classes=2,
+                                   random_state=42)
+        
+        class_column = 'class'
+        positive_class_label = 1
+        probability_threshold = 0.1
+        
+        test_data = pd.DataFrame(data[0], columns = ['col1','col2','col3','col4'])
+        test_data[class_column] = pd.Series(data[1])
 
-        test_data = pd.read_csv(test_path)
-
-        algo = IzdapAlgo(0.1)
+        algo = IzdapAlgo(probability_threshold)
         algo.fit(test_data, class_column=class_column, positive_class_label=positive_class_label)
 
         rules = algo.get_rules()
