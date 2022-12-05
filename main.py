fcaeae7229703d7122bb4af499615962859e3c38
levshun <dmitry.levshun@gmail.com>
2022-12-05 13:10:54 +0300

    Datasets for examples

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ import aopssop
 
 
 if __name__ == '__main__':
-    print(aopssop.posnd('Hello World!'))
-    # print(aopssop.izsnd('Hello World!'))
-    # print(aopssop.aossop('Hello World!'))
-    # print(aopssop.apssop('Hello World!'))
+    print('Hello World!')
 
