517884151b7a0f7310bde04d55404c2b60aa49ea
kuroboloid <kuroboloid@users.noreply.github.com>
2022-12-09 15:36:13 +0300

    Edit
    Signed-off-by: kuroboloid <kuroboloid@users.noreply.github.com>

--- examples/izdap_and_appssop_examples.py
+++ examples/izdap_and_appssop_examples.py
@@ -69,7 +69,20 @@ class DataLoaderAndPreprocessor:
             self.delete_features()
 
             if use_izdap:
-                self.izdap()
+                self.izdap(class_column='Alarm')
+
+            self.delete_labels()
+            self.features_names = self.data.columns.values
+            return self.data
+
+        elif dataset_name == 'hai':
+            self.data = pd.read_csv(self.DATA_PATH + 'HAI_test2.csv.zip')
+            self.drop_features = ['timestamp']
+            self.drop_labels = ['Attack']
+            self.delete_features()
+
+            if use_izdap:
+                self.izdap(class_column='Attack')
 
             self.delete_labels()
             self.features_names = self.data.columns.values
@@ -79,9 +92,10 @@ class DataLoaderAndPreprocessor:
             print('Unknown dataset name')
             exit()
 
-    def izdap(self):
+    def izdap(self, class_column, positive_class_label=1):
         algo = IzdapAlgo(0.1)
-        algo.fit(self.data, class_column='Alarm', positive_class_label=1)
+        algo.fit(self.data, class_column=class_column,
+                 positive_class_label=positive_class_label)
         rules = algo.get_rules()
         self.data = algo.transform(self.data)
 
@@ -157,50 +171,52 @@ def example_appsop_model_training(logname='example'):
     In this example, forecasting model and  normalization model are trained on the dataset.
     """
     logfile = logname + '_training_res.log'
-    result = {'test': logfile, 'data_size': PDATA.features_matrix.shape[0]}
 
-    LOG0.create(logfile, rewrite=True)
-    LOG0.run = True
-    thread1 = Thread(target=LOG0.daemon_logger)
-    thread1.start()
-
-    LOG0.event_init(text='Start with dataset "')
-
-    LOG0.event_init(event_name='preproc', text='Input data splitting')
+    # LOG0.create(logfile, rewrite=True)
+    # LOG0.run = True
+    # thread1 = Thread(target=LOG0.daemon_logger)
+    # thread1.start()
+    #
+    # LOG0.event_init(text='Start with dataset "')
+    #
+    # LOG0.event_init(event_name='preproc', text='Input data splitting')
     train, test = train_test_split(PDATA.features_matrix, train_size=0.9)
-    LOG0.event_init(event_name='preproc',
-                    text='Data is divided into train and test samples of length {0} and {1}'.format(len(train),
-                                                                                                    len(test)))
 
-    LOG0.event_init(event_name='norm', text='Normalization model training')
-    normalization_model = DataScaler(scaler_path=PDATA.normalization_model_path)
-    normalization_model.fit(train)
-    LOG0.event_init(event_name='norm', text='Data normalization')
-    scaled_train = normalization_model.transform(train)
-    LOG0.event_init(event_name='norm', text='Data normalized')
-
-    LOG0.event_init(event_name='prepare', text='Forecasting model creation:' + PDATA.forecasting_model_path)
-
-    forecasting_model = AIForecaster(n_epochs=3,
-                                     time_window_length=PDATA.time_window_length,
-                                     n_features=len(PDATA.features_names),
-                                     model_path=PDATA.forecasting_model_path,
-                                     )
-
-    LOG0.event_init(event_name='prepare', text='Data generator creation')
-    train_generator = forecasting_model.data_to_generator(scaled_train)
-    LOG0.event_init(event_name='prepare', text='Data generator created')
-
-    LOG0.event_init(event_name='train', text='Model training')
-    LOG0.show_off()
-    result['mse'] = forecasting_model.train(train_generator)
-    LOG0.show_on()
-    LOG0.event_init(event_name='train', text='Training completed')
-
-    LOG0.run = False
-    thread1.join()
-    LOG0.close()
-
+    result = {'test': logfile, 'data_size': train.shape}
+    #
+    # LOG0.event_init(event_name='preproc',
+    #                 text='Data is divided into train and test samples of length {0} and {1}'.format(len(train),
+    #                                                                                                 len(test)))
+    #
+    # LOG0.event_init(event_name='norm', text='Normalization model training')
+    # normalization_model = DataScaler(scaler_path=PDATA.normalization_model_path)
+    # normalization_model.fit(train)
+    # LOG0.event_init(event_name='norm', text='Data normalization')
+    # scaled_train = normalization_model.transform(train)
+    # LOG0.event_init(event_name='norm', text='Data normalized')
+    #
+    # LOG0.event_init(event_name='prepare', text='Forecasting model creation:' + PDATA.forecasting_model_path)
+    #
+    # forecasting_model = AIForecaster(n_epochs=3,
+    #                                  time_window_length=PDATA.time_window_length,
+    #                                  n_features=len(PDATA.features_names),
+    #                                  model_path=PDATA.forecasting_model_path,
+    #                                  )
+    #
+    # LOG0.event_init(event_name='prepare', text='Data generator creation')
+    # train_generator = forecasting_model.data_to_generator(scaled_train)
+    # LOG0.event_init(event_name='prepare', text='Data generator created')
+    #
+    # LOG0.event_init(event_name='train', text='Model training')
+    # LOG0.show_off()
+    # result['mse'] = forecasting_model.train(train_generator)
+    # LOG0.show_on()
+    # LOG0.event_init(event_name='train', text='Training completed')
+    #
+    # LOG0.run = False
+    # thread1.join()
+    # LOG0.close()
+    LOG0.filename = 'appsop_logs/' + logfile
     result.update(LOG0.get_resources(event_name='train'))
 
     print('Done')
@@ -230,6 +246,11 @@ def example_appsop_forecasting(logname='example', train_size=0.9,
     LOG0.event_init(text='Start with dataset "')
     LOG0.event_init(event_name='preproc', text='Input data splitting')
     train, test = train_test_split(PDATA.features_matrix, train_size=0.9)
+
+    test = test[:1000]
+
+    result = {'test': logfile, 'data_size': test.shape}
+
     LOG0.event_init(event_name='preproc',text='Data is divided into train and test samples of length {0} and {1}'.format(len(train),
                                                                                                     len(test)))
     LOG0.event_init(event_name='norm', text='Normalization model open')
@@ -326,7 +347,6 @@ def example_join_test_smart_crane():
             PDATA.forecasting_model_path = Loader.forecasting_model_path
             PDATA.normalization_model_path = Loader.normalization_model_path
 
-
             for ex in [str(i) + '_' for i in range(3)]:
                 logname = ex + dataset_name + '_' + test_name
                 if ex == '0_':
@@ -336,26 +356,39 @@ def example_join_test_smart_crane():
                 if ex == '2_':
                     result = result.append(example_appsop_forecasting(logname=logname, apriori=False), ignore_index=True)
     result = result.sort_values(by='test').reset_index(drop=True)
-    result.to_csv('appsop_logs/join_test_smart_crane.csv', index=False, sep=';', decimal=',')
+    result.to_csv('appsop_logs/join_test_' + dataset_name + '.csv', index=False, sep=';', decimal=',')
 
 
 def example_join_test_hai():
     PDATA.time_window_length = 10
-
     result = pd.DataFrame(columns=['test', 'data_size', 'mse', 'mae', '1-mae',
                                    'duration_sec', 'cpu%_min', 'cpu%_mean', 'cpu%_max',
                                    'ram_mb_min', 'ram_mb_mean', 'ram_mb_max'
                                    ])
 
     dataset_name = 'hai'
-    PDATA.features_matrix = Loader(dataset_name, use_izdap=False)
-    PDATA.features_names = Loader.features_names
-    PDATA.forecasting_model_path = Loader.forecasting_model_path
-    PDATA.normalization_model_path = Loader.normalization_model_path
+    for test_name, use_izdap in {'appsop_izdap0': False,
+                                 'appsop_izdap1': True}.items():
+
+        Loader = DataLoaderAndPreprocessor(test_name=test_name)
+        PDATA.features_matrix = Loader(dataset_name, use_izdap=use_izdap)
+        PDATA.features_names = Loader.features_names
+        PDATA.forecasting_model_path = Loader.forecasting_model_path
+        PDATA.normalization_model_path = Loader.normalization_model_path
+
+        for ex in [str(i) + '_' for i in range(3)]:
+            logname = ex + dataset_name + '_' + test_name
+            if ex == '0_':
+                result = result.append(example_appsop_model_training(logname=logname), ignore_index=True)
+            if ex == '1_':
+                result = result.append(example_appsop_forecasting(logname=logname), ignore_index=True)
+            if ex == '2_':
+                result = result.append(example_appsop_forecasting(logname=logname, apriori=False), ignore_index=True)
+    result = result.sort_values(by='test').reset_index(drop=True)
+    result.to_csv('appsop_logs/join_test_' + dataset_name + '.csv', index=False, sep=';', decimal=',')
 
 
 if __name__ == '__main__':
-    example_join_test_smart_crane()
-    Loader = DataLoaderAndPreprocessor(test_name=test_name)
+    example_join_test_hai()
 
 
