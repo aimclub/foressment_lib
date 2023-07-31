925c3cf80379d0703c0704a1f6bf5c845643e701
kuroboloid <kuroboloid@users.noreply.github.com>
2023-07-31 16:19:46 +0300

    Change loader

--- foressment_ai/forecasting/forecaster_ai/loader.py
+++ foressment_ai/forecasting/forecaster_ai/loader.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 import numpy as np
+import configparser
 from foressment_ai import RulesExtractor
 
 class DataLoaderAndPreprocessorDefault:
@@ -19,90 +21,149 @@ class DataLoaderAndPreprocessorDefault:
     :param data: Data feature matrix
     :type data: pandas.DataFrame
     """
-    def __init__(self, dataset_name, mode=1, suf=''):
+    def __init__(self, dataset_name, nrows=None, suf='', data_configs_path="../datasets/configs", label_format='binary'):
         """
         Initializing
 
         :param dataset_name: Data set name
-        :type dataset_name: srting
+        :type dataset_name: string
 
         :param mode: Boot mode, for developers
         :type mode: int
         """
 
-        DATA_PATH = "../datasets/"
+        self.dataset_name = dataset_name
 
-        self.features_names = []
-        self.drop_features = []
-        self.categorical_features = []
-        self.data = None
+        self.__load_data__(data_configs_path, label_format, nrows)
 
-        if not os.path.exists('forecaster_models/'):
-            os.makedirs('forecaster_models/')
+        # if not os.path.exists('forecaster_models/'):
+        #     os.makedirs('forecaster_models/')
 
-        self.forecasting_model_path = 'forecaster_models/forecasting_model_' + dataset_name + suf
+        # self.forecasting_model_path = 'forecaster_models/forecasting_model_' + dataset_name + suf
         self.normalization_model_path = 'forecaster_models/scaler_' + dataset_name + suf + '.pkl'
 
-        if dataset_name == 'hai':
-            DATA_PATH = DATA_PATH + 'hai-22.04/train'
+    def __load_data__(self, data_configs_path, label_format, nrows):
+        """
+        Import data with configuration file.
+        """
+        config = configparser.ConfigParser()
 
-            if mode == 0:
-                self.data = pd.read_csv(DATA_PATH + '/train1.csv', nrows=10000)
-
-            else:
-                self.data = pd.DataFrame()
-                for file in os.listdir(DATA_PATH):
-                    self.data = pd.concat([self.data, pd.read_csv(DATA_PATH + '/' + file)],
-                                           ignore_index=True)
-
-            self.drop_features = ['timestamp', 'Attack']
-            self.preprocessing()
-            self.features_names = self.data.columns.values
-
-        elif dataset_name == 'smart_crane':
-            self.data = pd.read_csv(DATA_PATH + 'IEEE_smart_crane.csv')
-
-            if mode not in range(1, 9):
-                print('Wrong cycle number')
-                exit()
-            self.data = self.data[self.data['Cycle'] == mode]
-
-            self.drop_features = ['Date', 'Alarm', 'Cycle']
-            self.preprocessing()
-            self.features_names = self.data.columns.values
-
-        elif dataset_name == 'test':
-            self.data = pd.DataFrame(np.random.randint(0, 100, size=(100, 4)),
-                                     columns=list('ABCD'))
-            self.features_names = self.data.columns.values
-
-        else:
-            print('Unknown dataset name')
+        try:
+            config.read(data_configs_path + '/' + self.dataset_name + '.ini')
+        except:
+            print('Unknown dataset name!')
             exit()
 
-    def preprocessing(self):
-        """
-        Data feature preprocessing
-        """
+        dataset_path = config.get('Params', 'data_path')
+        dataset_filename = config.get('Params', 'filename',  fallback='')
+        sep = config.get('Params', 'sep',  fallback=',')
+        decimal = config.get('Params', 'decimal', fallback='.')
+
+        if dataset_filename:
+            self.data = pd.read_csv(dataset_path + '/' + dataset_filename, sep=sep, decimal=decimal, nrows=nrows)
+        else:
+            for file in os.listdir(dataset_path):
+                self.data = pd.concat([self.data, pd.read_csv(dataset_path  + '/' + file, sep=sep, decimal=decimal)],
+                                      ignore_index=True)
+                if nrows:
+                    if self.data.shape[0] > nrows:
+                        self.data = self.data.loc[:nrows]
+                        break
+
+        self.data.columns = [c.strip() for c in self.data.columns]
+
+        self.timestamp_feature = config.get('Features', 'timestamp_feature', fallback=None)
+        self.binaries_features = self.__config_get_list__(config, 'binaries_features')
+        self.drop_features = self.__config_get_list__(config, 'drop_features')
+        self.categorical_features = self.__config_get_list__(config, 'categorical_features')
+        self.important_features = self.__config_get_list__(config, 'important_features')
+
+        if self.timestamp_feature:
+            self.timestamps = self.data[self.timestamp_feature].squeeze()
+
+            time_format = config.get('Other', 'time_format', fallback='')
+            timestamp_unit = config.get('Other', 'timestamp_unit', fallback='')
+
+            if time_format:
+                self.timestamps = self.str_data_to_time(self.timestamps,
+                                                        time_format=time_format)
+            if timestamp_unit:
+                self.timestamps = self.float_data_to_time(self.timestamps, timestamp_unit)
+
+            self.data = self.data.drop(self.timestamp_feature, axis=1)
+
+        label_section = 'Labels.' + label_format
+        label_config = config[label_section]
+
+        self.label_feature = label_config.get('label', fallback=None)
+        normal_label = label_config.get('normal_label', fallback='Normal')
+        self.labels = self.data[self.label_feature]
+        self.data = self.data.drop(self.label_feature, axis=1)
+
+        if not is_numeric_dtype(self.labels):
+            self.__encoding_labels__(normal_label)
+
+        self.drop_features = [f for f in self.drop_features if f in self.data.columns]
         if len(self.drop_features) > 0:
             self.data = self.data.drop(self.drop_features, axis=1)
 
-        if len(self.categorical_features) > 0:
-            for feature in self.categorical_features:
-                if 'ip' in feature:
-                    encoded_feature_data = pd.DataFrame([x.split('.')
-                                                         if x != '0' else [0, 0, 0, 0]
-                                                         for x in self.data[feature].tolist()])
+    @staticmethod
+    def __config_get_list__(config, feature_name):
+        try:
+            return config.get('Features', feature_name).replace('\n', '').split(',')
+        except:
+            return []
 
+    @staticmethod
+    def str_data_to_time(data, time_format):
+        '''
+        :param data: timestamp column (pd.Series)
+        :return:
+        '''
+        data = data.map(lambda x: x.strip() if not pd.isnull(x) else x)
+        data = pd.to_datetime(data, format=time_format)
+        return data
+
+    @staticmethod
+    def float_data_to_time(data, timestamp_unit):
+        '''
+        :param data: timestamp column (pd.Series)
+        :return:
+        '''
+        data = pd.to_datetime(data, unit=timestamp_unit)
+        return data
+
+    def __encoding_labels__(self, normal_label):
+        labels_classes = sorted(self.labels.unique().tolist())
+        labels_dictionary = {}
+
+        if normal_label in labels_classes:
+            labels_dictionary[normal_label] = 0
+            labels_classes.remove(normal_label)
+
+        for i, label in enumerate(labels_classes):
+            labels_dictionary[label] = i
+
+        self.labels = self.labels.map(lambda x: labels_dictionary[x])
+
+    def categorical_feature_encoding(self):
+        """
+        Data categorical feature encoding
+        """
+        if len(self.categorical_features) > 0:
+            new_categorical_features = []
+            for feature in self.categorical_features:
+                try:
+                    self.data[feature] = self.data[feature].astype(str)
+                    categorical_feature_values = self.data[feature].unique().tolist()
+                    if ('0.0' in categorical_feature_values) and ('0' in categorical_feature_values):
+                        self.data[feature] = self.data[feature].map(lambda x: '0'if x == '0.0'else x)
+                    encoded_feature_data = pd.get_dummies(self.data[feature])
                     for col in encoded_feature_data.columns:
-                        encoded_feature_data = encoded_feature_data.rename(columns={col: feature + '_' + str(col)})
-                else:
-                    try:
-                        encoded_feature_data = pd.get_dummies(self.data[feature])
-                        for col in encoded_feature_data.columns:
-                            encoded_feature_data = encoded_feature_data.rename(columns={col: feature + '_' + col})
-                    except:
-                        encoded_feature_data = pd.DataFrame()
+                        encoded_feature_data = encoded_feature_data.rename(columns={col: feature + '_' + col})
+                        new_categorical_features.append(feature + '_' + col)
+                except:
+                    encoded_feature_data = pd.DataFrame()
 
                 if not encoded_feature_data.empty:
                     old_data_columns = self.data.columns.tolist()
@@ -116,8 +177,8 @@ class DataLoaderAndPreprocessorDefault:
                 else:
                     print('Too many values for categorical feature "' + feature +'". Delete feature from data')
                     self.data = self.data.drop(feature, axis=1)
+            self.categorical_features = new_categorical_features
         self.data = self.data.fillna(0)
-
 
     def train_test_split(self, train_size=0.9):
         """
